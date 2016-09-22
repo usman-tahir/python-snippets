93ff61fecfbf989c6c5ad90350123c9a95529463
Usman Tahir <tahir.usman.ali94@outlook.com>
2016-09-22 10:07:56 -0400

    added comments and small improvements to character_input.py

--- character_input.py
+++ character_input.py
@@ -1,10 +1,15 @@
 
+# Input for name (defaults to string)
 name = raw_input("Please enter your name: ")
-age = input("Please enter your age: ")
 
-print("Your name is %s and you are %s years old." % (name, str(age)))
-until_100 = 100 - int(age)
+#Input for age (parsed as an integer)
+age = int(input("Please enter your age: "))
 
+# print("Your name is %s and you are %s years old." % (name, str(age)))
+print("Your name is " + name + " and you are " + str(age) + " years old.")
+until_100 = 100 - age
+
+# Determine which message to output
 if until_100 <= 100 and until_100 >= 0:
   print("You will turn 100 in %s years." % (str(until_100)))
 else:
