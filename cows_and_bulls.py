9dfa2d2709b509fc924a2f487ac52ee7bb0661f3
Usman Tahir <tahir.usman.ali94@outlook.com>
2016-09-25 16:24:20 -0400

    practicepython.org day 18 code

---
+++ cows_and_bulls.py
@@ -0,0 +1,33 @@
+import random
+import sys
+
+
+def cows_and_bulls_printout(actual, guess):
+  string_actual = str(actual)
+  string_guess = str(guess)
+  cows_and_bulls = [0, 0]
+
+  for x in range(len(string_actual)):
+    if string_actual[x] == string_guess[x]:
+      cows_and_bulls[0] += 1
+    elif string_actual[x] != string_guess[x]:
+      cows_and_bulls[1] += 1
+  
+  if cows_and_bulls[0] == 4:
+    print("\nYou guessed the number! It was: %s" % (string_actual))
+    return(False)
+  else:
+    print("You have %s cows and %s bulls." % (str(cows_and_bulls[0]), str(cows_and_bulls[1])))
+    return(True)
+
+# The number must be 4 digits, so it needs to be between 1000 and 9999
+generated = random.randint(1000, 10000)
+# print(str(generated))
+flag = True
+guesses = 0
+
+while flag == True:
+  guess = int(raw_input("Please enter your 4 digit guess: "))
+  guesses += 1
+  flag = cows_and_bulls_printout(generated, guess)
+print("It took you %s guess(es) to figure out the number." % (str(guesses)))
