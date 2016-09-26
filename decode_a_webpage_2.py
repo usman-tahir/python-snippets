416d7c4b198679da0eab824860b0604d857aa711
Usman Tahir <tahir.usman.ali94@outlook.com>
2016-09-25 20:32:21 -0400

    modified day 19 code with comments and small code changes

--- decode_a_webpage_2.py
+++ decode_a_webpage_2.py
@@ -2,21 +2,26 @@ import requests
 from bs4 import BeautifulSoup
 
 def get_article_portions(soup):
+  # Find the heading with the specific class
   heading = soup.find("div", {"class": "dek"})
   if heading:
     yield heading.text
   
+  # Locate all the paragraphs from the webpage
   for p in soup.find_all("p", {"class": ""}):
     yield p.text
 
 def read_page(address):
-  page = requests.get(address)
+  page = requests.get(address) # Open the webpage
   soup = BeautifulSoup(page.text, "html.parser")
 
+  # Get each paragraph, and allow the user to continue by hitting 'enter'
   for s in get_article_portions(soup):
     print "\n%s" % s.encode("utf-8")
     raw_input("\nPress enter to continue")
+  # Once the article ends
   print("End of article")
 
 if __name__ == "__main__":
-  read_page("http://www.vanityfair.com/style/society/2014/06/monica-lewinsky-humiliation-culture")
+  url = "http://www.vanityfair.com/style/society/2014/06/monica-lewinsky-humiliation-culture"
+  read_page(url)
