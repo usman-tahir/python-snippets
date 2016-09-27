fb5958da7bb5c67d5dddb7399573b5f8f1303602
Usman Tahir <tahir.usman.ali94@outlook.com>
2016-09-27 08:52:50 -0400

    successful scripting of a basic web scraper

---
+++ web_scraping/scraper.py
@@ -0,0 +1,17 @@
+import urllib2
+
+def download(url, num_retries = 2):
+	print("Downloading: ", url)
+	try:
+		html = urllib2.urlopen(url).read()
+	except urllib2.URLError as e:
+		print("Download error: ", e.reason)
+		html = None
+		if num_retries > 0:
+			if hasattr(e, 'code') and 500 <= e.code < 600:
+				# Recursively retry 5xx HTTP errors
+				return download(url, num_retries - 1)
+	finally:
+		return html
+
+print(download("http://www.github.com/usman-tahir"))
