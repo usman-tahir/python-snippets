6dd169b96ec3c1cf8cf49276e0ca1ab5484c9794
usman-tahir <tahir.usman.ali94@outlook.com>
2016-12-20 16:29:37 -0500

    added a new directory with a quiz creator class

---
+++ python-games/quiz-creator/quiz.py
@@ -0,0 +1,27 @@
+
+class Quiz:
+  def __init__(self, questions_file, answers_file):
+    self.questions_file = questions_file
+    self.answers_file = answers_file
+    self.parsed_questions = Quiz.parse_data(self, self.questions_file)
+    self.parsed_answers = Quiz.parse_data(self, self.answers_file)
+  
+  def parse_data(self, file_path):
+    data = open(file_path)
+    parsed_data = data.readlines()
+    for p in parsed_data:
+      p = p.rstrip("\n")
+    
+    return parsed_data
+  
+  def get_questions(self):
+    return self.parsed_questions
+  
+  def get_answers(self):
+    return self.parsed_questions
+  
+  def set_questions(self, questions_file):
+    self.parsed_questions = Quiz.parse_data(questions_file)
+  
+  def set_answers(self, answers_file):
+    self.parsed_answers = Quiz.parse_data(answers_file)
