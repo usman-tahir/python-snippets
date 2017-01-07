68422a42ba1a6e2c2f83376aae176a7fc31feb9b
usman-tahir <tahir.usman.ali94@outlook.com>
2017-01-06 21:04:52 -0500

    added user input functionality for quiz names/quiz problems; added a test file with arithmetic problems

--- python-games/flashcards/quiz_main.py
+++ python-games/flashcards/quiz_main.py
@@ -1,7 +1,20 @@
 
+import os.path
 import flashcards
 import quiz
 import random
+
+def get_quiz_file():
+	quiz_name = input("Enter a name for the quiz: ")
+	user_file = input("Enter the file name (Example: 'problems.txt'): ")
+	if os.path.isfile("./" + str(user_file)):
+		if len(quiz_name) > 0:
+			return quiz.Quiz(quiz_name, "./" + str(user_file))
+		else:
+			print("You entered a blank quiz name.")
+	else:
+		print("Please re-run this script and enter a valid quiz file.")
+	
 
 def start_quiz(quiz):
 	correct = 0
@@ -13,7 +26,7 @@ def start_quiz(quiz):
 		current_question = questions[i].get_question()
 		current_answer = questions[i].get_answer()
 
-		user_answer = raw_input("\n" + str(current_question) + "? ")
+		user_answer = input("\n" + str(current_question) + "? ")
 		if (user_answer.lower() == current_answer.lower()):
 			print("Correct!")
 			correct += 1
@@ -28,7 +41,7 @@ def start_quiz(quiz):
 
 
 def main():
-	q = quiz.Quiz("State Capitals Quiz", "./state_capitals.txt")
+	q = get_quiz_file()
 	correct = start_quiz(q)
 	print("\nYou got %s out of %s correct." % (str(correct[0]), str(correct[1])))
 
