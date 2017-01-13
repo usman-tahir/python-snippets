8e1549eb2f9429761b168da24eb72bedd842aecf
Usman Tahir <tahir.usman.ali94@outlook.com>
2017-01-12 23:52:07 -0500

    modified the twitter API tweet retriever code (needs debugging)

--- python-tools/twitter-api-app/config.py
+++ python-tools/twitter-api-app/config.py
@@ -1,7 +1,4 @@
-#Please put the values here.
-key=""
-secret=""
-access_key=""
-access_secret=""
-
-
+key = "lTEDFHHtK8UrPegHvs9N9scYg"
+secret = "6nCtdyhpU3gMMeSwZaDnOlPalOLaZp6vdrYwkdiB7FjCpXVpn2"
+access_key = "817046760837312513-b8vRXrDqgKGT6oceoqUJp8tIVm8e5im"
+access_secret = "OaX2tUnUo540HmNJTbg5WGUoBSqZvU1jfrDQWQLavBPPO"
