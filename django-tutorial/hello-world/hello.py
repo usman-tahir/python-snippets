1aaaeaff138d36bf2e67f9d8b02adfe82ef8ea5d
Usman Tahir <tahir.usman.ali94@outlook.com>
2017-03-26 18:32:14 -0400

    started a Django snippets directory; created a basic Hello World Django static page

---
+++ django-tutorial/hello-world/hello.py
@@ -0,0 +1,29 @@
+import sys
+
+from django.conf import settings
+
+from django.conf.urls import url
+from django.http import HttpResponse
+
+from django.core.management import execute_from_command_line
+
+settings.configure(
+    DEBUG = True,
+    SECRET_KEY = 'secret',
+    ROOT_URLCONF = __name__,
+    MIDDLEWARE_CLASSES = (
+        'django.middleware.common.CommonMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
+        'django.middleware.clickjacking.XFrameOptionsMiddleware'
+    ),
+)
+
+def index(request):
+    return HttpResponse('Hello World')
+
+urlpatterns = (
+    url(r'^$', index),
+)
+
+if __name__ == "__main__":
+    execute_from_command_line(sys.argv)
