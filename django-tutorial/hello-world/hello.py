c0c969228abd5f31032d2a6866ca74eb4f32b0be
Usman Tahir <tahir.usman.ali94@outlook.com>
2017-03-27 07:27:45 -0400

    created an ALLOWED_HOSTS setting to allow HTTP requests from 'localhost' and 'example.com'

--- django-tutorial/hello-world/hello.py
+++ django-tutorial/hello-world/hello.py
@@ -12,10 +12,12 @@ from django.core.management import execute_from_command_line
 
 DEBUG = os.environ.get('DEBUG', 'on') == 'on'
 SECRET_KEY = os.environ.get('SECRET_KEY', os.urandom(32))
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
 
 settings.configure(
     DEBUG = DEBUG,
     SECRET_KEY = SECRET_KEY,
+    ALLOWED_HOSTS = ALLOWED_HOSTS,
     ROOT_URLCONF = __name__,
     MIDDLEWARE_CLASSES = (
         'django.middleware.common.CommonMiddleware',
