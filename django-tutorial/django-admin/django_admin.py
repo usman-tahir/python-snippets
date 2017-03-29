48f3cc1dfc7534e474b6bfeee332951536ccf76b
Usman Tahir <tahir.usman.ali94@outlook.com>
2017-03-28 22:52:04 -0400

    added functionality for a basic view (images)

--- django-tutorial/django-admin/django_admin.py
+++ django-tutorial/django-admin/django_admin.py
@@ -26,11 +26,16 @@ settings.configure(
     ),
 )
 
+def placeholder(request, width, height):
+    # Insert the rest of the view here
+    return HttpResponse('OK')
+
 def index(request):
     return HttpResponse('Hello World')
 
 urlpatterns = (
-    url(r'^$', index),
+    url(r'^image/(?P<width>[0-9]+)x(?P<height>[0-9]+)/$', placeholder, name = 'placeholder')
+    url(r'^$', index, name = 'homepage'),
 )
 
 application = get_wsgi_application()
